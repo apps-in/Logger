1797ec91afb0dd61ad0c0d440262ea44650944af
Igor Nepomniashchyi <nepomniashchyi.igor@gmail.com>
2020-03-03 16:59:42 +0200

    V 0.6.3

--- android_logger/src/main/java/apps/in/android_logger/ServerApi.java
+++ android_logger/src/main/java/apps/in/android_logger/ServerApi.java
@@ -1,11 +1,12 @@
 package apps.in.android_logger;
 
+import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface ServerApi {
 
     @POST("Api/PostLog")
-    void sendLog(@Body LogItem logItem);
+    Call<Void> sendLog(@Body LogItem logItem);
 
 }
